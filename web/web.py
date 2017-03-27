e9387b7a161ef187deab4b598ae42dc5061fa8d3
DESKTOP-F6DG3P0\HP <dragon15098@gmail.com>
2017-03-27 12:08:04 +0700

    update_image

--- web/web.py
+++ web/web.py
@@ -1,29 +1,10 @@
-from flask import *
 from datetime import *
-app = Flask(__name__)
+from flask import *
+import mlab
+from models.images import Image
 
-list_food_1 = [
-    {
-        "arc" : "http://68.media.tumblr.com/fdce8d90185f8f38a5f36a69b198a271/tumblr_ojqkfv1h7c1qbd81ro1_1280.jpg",
-        "title" : "12343 by Đinh Văn Linh",
-        "tags" : "#girl xinh#pretty girl#vietnam girl#beautiful girl photo#photography"
-    },
-    {
-        "arc" : "http://68.media.tumblr.com/6707567f4007a0ebb6e342f37692da0b/tumblr_of8kzf8gMp1qbd81ro1_1280.jpg",
-        "title" : "lightstudio | 0966726996 by Leo White",
-        "tags" : "#girl xinh#pretty girl#vietnam girl#beautiful girl photo#photography"
-    },
-    {
-        "arc" : "http://68.media.tumblr.com/b2546075aa96c52eb287b44da313ac29/tumblr_of8kzbVZmv1qbd81ro1_1280.jpg",
-        "title" : "DSC_2180 by mrSun_vn",
-        "tags" : "#girl xinh#pretty girl#vietnam girl#beautiful girl photo#photography"
-    },
-    {
-        "arc" : "http://68.media.tumblr.com/7111feb3f9b20cadb1fca92d4aaf62f9/tumblr_of8kz3Im0h1qbd81ro1_1280.jpg",
-        "title" : "IMG_7340 by Hải Nguyễn | Tell 0902990341",
-        "tags" : "#girl xinh#pretty girl#vietnam girl#beautiful girl photo#photography"
-    }
-    ]
+app = Flask(__name__)
+mlab.connect()
 
 @app.route('/')
 def hello_world():
@@ -41,26 +22,58 @@ def login():
 
 @app.route('/food')
 def food():
-    return render_template("food.html", list_food = list_food_1)
+    return render_template("food.html", list_food = Image.objects())
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-
 @app.route('/css')
 def css():
     return render_template("css_demo.html")
-
 
 @app.route('/w3css')
 def w3css():
     return render_template("w3css.html")
 
+@app.route('/add_image', methods=["GET", "POST"])
+def add():
+    if(request.method == "GET"):
+        return render_template("add_image.html")
+    if(request.method == "POST"):
+        new_image = Image()
+        new_image.src = request.form["source"]
+        new_image.title = request.form["title"]
+        new_image.description = request.form["description"]
+        new_image.save()
+        return render_template("add_image.html")
+
+@app.route('/delete_image', methods=["GET", "POST"])
+def delete():
+    if(request.method == "GET"):
+        return render_template("delete_image.html")
+    if(request.method == "POST"):
+        new_image = Image.objects(title=request.form["title"]).first()
+        if new_image is not None:
+            new_image.delete()
+        return render_template("delete_image.html")
+
+@app.route('/update_image', methods=["GET", "POST"])
+def update():
+    if (request.method == "GET"):
+        return render_template("update_image.html")
+    if (request.method == "POST"):
+        new_image = Image.objects(title=request.form["title"]).first()
+        if new_image is not None:
+            new_image.src = request.form["new_source"]
+            new_image.description = request.form["new_description"]
+            new_image.save()
+        return render_template("update_image.html")
+
 
 @app.route('/foodblog')
 def foodblog():
-    return render_template("foodblog.html")
+    return render_template("foodblog.html", list_food = Image.objects())
 
 if __name__ == '__main__':
     app.run()
